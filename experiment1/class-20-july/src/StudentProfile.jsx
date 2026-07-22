5ef9a8c21cb505a626ab8000f24e9c08ac6b0524
maaz <mxansari007@gmail.com>
2026-07-22 21:08:10 +0530

    Update class 20 july student profile

--- experiment1/class-20-july/src/StudentProfile.jsx
+++ experiment1/class-20-july/src/StudentProfile.jsx
@@ -1,18 +1,46 @@
 // import './style.css'
+import {useState} from 'react';
+
 
 const StudentProfile = (students) =>{
-    let size = 10
-    const style1 = {
-        border: '1px solid #ccc',
-        padding: '10px',
-        margin: `${size}px`,
-        backgroundColor: '#f9f9f9'
-    }
 
-  return <div style={style1}>
-    <p>Name : {students[0]?.name}</p>
-    <p>Course : Computer Science</p>
-    <p>Year : 2026</p>
+  let [theme, setTheme] = useState('Dark');
+  let [fruits, setFruits] = useState(['Apple', 'Banana', 'Mango']);
+
+  let [student,setStudent] = useState({
+    name:'Alice',
+    class:'710',
+    course:'Fullstack'
+  })
+
+  const changeTheme = () =>{
+
+    setTheme((previous)=>previous === 'Dark' ? 'Light' : 'Dark');
+
+  }
+
+  const addFruit = () =>{
+    let randomFruit = [...fruits]
+
+    randomFruit.push('kiwi')
+
+    setFruits(randomFruit);
+  }
+
+  const changeCourse = ()=>{
+    let shallowObj = {...student, course:'CC'}
+    setStudent(shallowObj);
+  }
+
+  return <div>
+      <p>Theme : {theme}</p>
+      <p>Name: {student.name}</p>
+      <p>class : {student.class}</p>
+      <p>course: {student.course}</p>
+      <button onClick={changeTheme}>Change Theme</button>
+      <button onClick={addFruit}>Add Fruit</button>
+      <button onClick={changeCourse}>Change course</button>
+      <p>Fruits : {fruits.join(', ')}</p>
   </div>
 }
 
